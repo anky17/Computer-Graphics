0463717e6214e4981747a1c0e726232cc9fbebd3
Parakram <89983841+ParakramK@users.noreply.github.com>
2024-02-27 16:46:08 +0545

    cleanup

--- src/Lab5/Shearing.cpp
+++ src/Lab5/Shearing.cpp
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int maxX, maxY, midX, midY;
-
 typedef struct point {
   float x;
   float y;
@@ -22,14 +20,10 @@ void matMul(float a[3][3], float b[3][3], float mul[3][3]) {
   }
 }
 
-void drawTriangle(Point P1, Point P2, Point P3) {
+void drawTriangle(Point P1, Point P2, Point P3, int mvCoeff, char *str) {
   line(P1.x, P1.y, P2.x, P2.y);
   line(P2.x, P2.y, P3.x, P3.y);
   line(P1.x, P1.y, P3.x, P3.y);
-}
-
-void normalize(Point P1, Point P2, Point P3, int mvCoeff, char *str) {
-  drawTriangle(P1, P2, P3);
   outtextxy(P3.x + mvCoeff, P3.y + mvCoeff, str);
 }
 
@@ -52,25 +46,21 @@ Point YShear(Point P, float shY) {
 }
 
 int main() {
-  Point P1 = {0, 0}, P2 = {0, 100}, P3 = {50, 100};
+  Point P1 = {50, 15}, P2 = {60, 105}, P3 = {150, 100};
   Point Pt1, Pt2, Pt3;
   int gdriver = DETECT, gmode, errorcode;
   initgraph(&gdriver, &gmode, (char *)"C:/TurboC3/BGI");
-  maxX = getmaxx();
-  maxY = getmaxy();
-  midX = maxX / 2;
-  midY = maxY / 2;
-
-  normalize(P1, P2, P3, -30, (char *)"Original");
+  drawTriangle(P1, P2, P3, 5, (char *)"Original");
+  // Y SHEARING
   Pt1 = YShear(P1, 2);
   Pt2 = YShear(P2, 2);
   Pt3 = YShear(P3, 2);
-
-  normalize(Pt1, Pt2, Pt3, -20, (char *)"Y Shear");
+  drawTriangle(Pt1, Pt2, Pt3, 5, (char *)"Y Shear");
+  // X SHEARING
   Pt1 = XShear(P1, 2);
   Pt2 = XShear(P2, 2);
   Pt3 = XShear(P3, 2);
-  normalize(Pt1, Pt2, Pt3, -20, (char *)"X Shear");
+  drawTriangle(Pt1, Pt2, Pt3, 5, (char *)"X Shear");
 
   getch();
   closegraph();
